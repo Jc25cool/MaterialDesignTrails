fdaaa74e52b5b51e6bbf8c16704a59fe864b7978
Basina <748638@cognizant.com>
2019-07-01 19:37:34 +0530

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,9 @@ export class AppComponent implements OnInit{
   marketPlaceHolderData ="Select Market";
   stationPlaceHolderData ="Select Station";
 
+  divOneContent = ['Content 1','Content 2', 'Content 3', 'Content 4'];
+  divTwoContent = ['Content 5','Content 6', 'Content 7', 'Content 8']
+
   marketDropdownList= [];
 
   stationDropdownList =[];
@@ -45,6 +48,58 @@ export class AppComponent implements OnInit{
   listOfSelectedStations(e){
     // console.log("kjghj")
     console.log("Just Send the data")
+  }
+
+  drop(ev) {
+    ev.preventDefault();
+    let flag = false;
+    var data = ev.dataTransfer.getData("text");
+    //console.log(document.getElementById(data).firstElementChild.innerHTML)
+    var dataContent= document.getElementById(data).firstElementChild.innerHTML.trim();
+    console.log(dataContent);
+
+    let indexPostionOne= this.divOneContent.indexOf(dataContent);
+    let indexPostionTwo = this.divTwoContent.indexOf(dataContent);
+
+    console.log(indexPostionOne, indexPostionTwo)
+
+    if(indexPostionOne == -1){
+      this.divOneContent.push(dataContent);
+      this.divTwoContent.splice(indexPostionTwo,1);
+    }
+    else if(indexPostionTwo == -1){
+       this.divTwoContent.push(dataContent);
+      this.divOneContent.splice(indexPostionOne,1);
+    }
+
+    // this.divOneContent.forEach((item, index)=>{
+    //   if(item.indexOf(dataContent) != -1){
+    //     this.divTwoContent.push(data);
+    //     this.divOneContent.splice(index,1);
+    //     flag= true;
+    //   }
+    // })
+
+    // if(flag == false){
+    //   this.divTwoContent.forEach((item, index)=>{
+    //   if(item.indexOf(dataContent) != -1){
+    //     this.divOneContent.push(data);
+    //     this.divTwoContent.splice(index,1);
+    //   }
+    // })
+    // }
+
+
+
+    //ev.target.appendChild(document.getElementById(data));
+  }
+
+  allowDrop(ev) {
+    ev.preventDefault();
+  }
+
+  drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
   }
 }
 
